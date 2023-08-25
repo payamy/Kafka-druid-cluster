d9438d3abee1e9b98bdd2e2a4c83f7d912c1b1f4
Payam <payamyarandi98@gmail.com>
2023-08-25 21:25:06 +0000

    cluster tested successfully

--- airflow/dags/stream_data.py
+++ airflow/dags/stream_data.py
@@ -2,6 +2,27 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
+from requests.auth import HTTPBasicAuth
+import requests
+import json
+import time
+
+
+class DruidDatasource:
+
+    @staticmethod
+    def load_json():
+        path = f'/opt/airflow/dags/kafka_ds_config.json'
+        try:
+            with open(path, "r") as config_file:
+                conf = json.load(config_file)
+        except FileNotFoundError:
+            raise FileNotFoundError('Config file not found')
+
+        return conf
+
+
+
 
 default_args = {
     'owner': 'Payam',
@@ -12,7 +33,24 @@ default_args = {
 
 
 def load_data():
-    pass
+    conf = DruidDatasource.load_json()
+    auth = HTTPBasicAuth('admin', 'druidAdminPassword')
+    res = requests.post(
+        url='http://router:8888/druid/indexer/v1/supervisor',
+        json=conf,
+        auth=auth,
+    )
+    time.sleep(60)
+    supervisor_id = json.loads(res._content)['id']
+    res_2 = requests.get(
+        url=f'http://router:8888/druid/indexer/v1/supervisor/{supervisor_id}/status',
+        auth=auth,
+    )
+    healthy = str(json.loads(res_2._content)['payload']["state"])
+    if healthy == "RUNNING":
+        print('Data loaded to Druid datasource')
+    else:
+        raise AirflowException('Submitting supervisor was unsuccessful')
 
 
 with DAG(
